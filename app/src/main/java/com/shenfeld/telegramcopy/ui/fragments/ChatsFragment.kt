762f2d245e2b8371f7a11adc662c68e6f4a0545e
Raum8 <shenfeldd@gmail.com>
2020-10-20 17:23:55 +0300

    Refactor project files (way to MVVM)

---
+++ app/src/main/java/com/shenfeld/telegramcopy/ui/fragments/ChatsFragment.kt
@@ -0,0 +1,11 @@
+package com.shenfeld.telegramcopy.ui.fragments
+
+import com.shenfeld.telegramcopy.R
+
+class ChatsFragment : BaseFragment(R.layout.fragment_chats) {
+
+    override fun onResume() {
+        super.onResume()
+
+    }
+}
---
+++ app/src/main/java/com/shenfeld/telegramcopy/ui/fragments/SettingsFragment.kt
@@ -0,0 +1,9 @@
+package com.shenfeld.telegramcopy.ui.fragments
+
+import com.shenfeld.telegramcopy.R
+
+class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
+    override fun onResume() {
+        super.onResume()
+    }
+}
